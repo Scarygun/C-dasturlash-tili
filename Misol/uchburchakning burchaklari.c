879a6383ca6a41bef9449b0c0bf8af489556a456
Scary Gungan <scarygun@ti-e5.skd.21-school.uz>
2025-01-08 23:30:24 +0500

    qwerty

---
+++ Misol/uchburchakning burchaklari.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Berilgan a = ");
+    scanf("%d", &a);
+    printf("Berilgan b = ");
+    scanf("%d", &b);
+    printf("Berilgan c = ");
+    scanf("%d", &c);
+
+    if (a > 0 && b > 0 && c > 0 && (a + b + c == 180)) {
+        printf("Natija: true\n");
+    } else {
+        printf("Natija: false\n");
+    }
+
+    return 0;
+}
